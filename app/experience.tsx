080da4e21166d8b01c7a76cef7bdf87af9b4d9dc
A-N1khil <nikhilanand1006@gmail.com>
2026-06-07 14:54:43 -0400

    Experience Init

---
+++ app/experience.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useLayoutEffect, useRef, type RefObject } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useCursor } from "@/app/CursorProvider";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Experience() {
+  const { hollowCursor, solidCursor } = useCursor();
+
+  // Wrapper over the timeline
+  // Also serves as the trigger for the scroll animation
+  const wrapperRef: RefObject<HTMLDivElement | null> = useRef<HTMLDivElement | null>(null);
+
+  // Ref for the experience title
+  const experienceTitleRef: RefObject<HTMLDivElement | null> = useRef<HTMLDivElement | null>(null);
+
+  // Ref for the experience subtitle
+  const experienceSubTitleRef: RefObject<HTMLDivElement | null> = useRef<HTMLDivElement | null>(null);
+
+  // Layout effect for animating the experience section title
+  useLayoutEffect(() => {
+    if (!experienceTitleRef.current) return;
+
+    const context = gsap.context(() => {
+      const wrapper: HTMLDivElement | null = wrapperRef.current;
+      if (!wrapper || !experienceTitleRef.current || !experienceSubTitleRef.current) return;
+
+      gsap.set(experienceTitleRef.current, {
+        opacity: 0,
+        y: 80,
+      });
+
+      ScrollTrigger.create({
+        trigger: wrapper,
+        start: "top 80%",
+        once: true,
+        onEnter: (): void => {
+          gsap
+            .timeline()
+            .to(experienceTitleRef.current, {
+              y: 0,
+              opacity: 1,
+              duration: 0.8,
+              ease: "power3.out",
+            })
+            .to(
+              experienceSubTitleRef.current,
+              {
+                y: 0,
+                opacity: 1,
+                duration: 0.8,
+                ease: "power3.out",
+              },
+              "-=0.35"
+            );
+        },
+      });
+    });
+
+    return (): void => context.revert();
+  });
+
+  return (
+    <>
+      <section id="experience" className="relative w-full overflow-hidden py-24">
+        {/* Experience Title */}
+        <div ref={experienceTitleRef} className="mb-10 px-8 text-center">
+          <p
+            className="text-5xl font-bold text-[var(--foreground)] [font-family:var(--font-arvo)]"
+            onMouseEnter={hollowCursor}
+            onMouseLeave={solidCursor}
+          >
+            Experience
+          </p>
+        </div>
+
+        {/* Experience Subtitle */}
+        <div
+          className="mb-10 px-8 text-center text-md italic text-zinc-500"
+          onMouseEnter={hollowCursor}
+          onMouseLeave={solidCursor}
+          ref={experienceSubTitleRef}
+        >
+          <p className="text-md italic text-zinc-500">
+            &quot;It is possible to fly without motors, but not without knowledge and skill.&quot;
+          </p>
+          <p>
+            - <span className="hover:underline hover:decoration-dotted">Wilbur Wright</span>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Landing from "@/app/landing";
 import Skills from "@/app/skills";
+import Experience from "./experience";
 
 export default function Home() {
-
-    return <>
-        <Landing />
-        <Skills />
-        </>;
+  return (
+    <>
+      <Landing />
+      <Skills />
+      <Experience />
+    </>
+  );
 }
